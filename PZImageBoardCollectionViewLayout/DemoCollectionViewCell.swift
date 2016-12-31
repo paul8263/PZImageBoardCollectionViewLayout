59f5291249fbe4db953d54d0cfa4f1d84b45e1d7
paulzhang <paul8263@gmail.com>
2016-12-31 16:44:14 +1100

    initial upload

---
+++ PZImageBoardCollectionViewLayout/DemoCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  DemoCollectionViewCell.swift
+//  PZImageBoardCollectionViewLayout
+//
+//  Created by Paul Zhang on 31/12/2016.
+//  Copyright © 2016 Paul Zhang. All rights reserved.
+//
+
+import UIKit
+
+class DemoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+}
---
+++ PZImageBoardCollectionViewLayout/PZImageBoardCollectionViewLayout.swift
@@ -0,0 +1,84 @@
+//
+//  PZImageBoardCollectionViewLayout.swift
+//  PZImageBoardCollectionViewLayout
+//
+//  Created by Paul Zhang on 31/12/2016.
+//  Copyright © 2016 Paul Zhang. All rights reserved.
+//
+
+import UIKit
+
+protocol PZImageBoardCollectionViewLayoutDelegate: class {
+    
+    func collectionView(_ collectionView: UICollectionView, layout: UICollectionViewLayout, heightForCellAtIndexPath indexPath: IndexPath) -> CGFloat
+}
+
+class PZImageBoardCollectionViewLayout: UICollectionViewLayout {
+    
+    weak var delegate: PZImageBoardCollectionViewLayoutDelegate? {
+        return collectionView?.delegate as? PZImageBoardCollectionViewLayoutDelegate
+    }
+    
+    var numberOfColumn = 2 {
+        didSet {
+            invalidateLayout()
+        }
+    }
+    
+    var contentWidth: CGFloat = 0.0 {
+        didSet {
+            invalidateLayout()
+        }
+    }
+    
+    var cellMargin: CGFloat = 10.0 {
+        didSet {
+            invalidateLayout()
+        }
+    }
+    
+    private var heightForColumn: [CGFloat] = []
+    
+    private var layoutAttributes: [UICollectionViewLayoutAttributes] = []
+
+    override func prepare() {
+        let columnWidth = (contentWidth - CGFloat(numberOfColumn + 1) * cellMargin ) / CGFloat(numberOfColumn)
+        heightForColumn = [CGFloat](repeating: cellMargin, count: numberOfColumn)
+        for index in 0..<collectionView!.numberOfItems(inSection: 0) {
+            let indexForColumn = heightForColumn.index(of: heightForColumn.min()!)!
+            
+            let indexPath = IndexPath(row: index, section: 0)
+            
+            let attr = UICollectionViewLayoutAttributes(forCellWith: indexPath)
+            
+            let offsetY = heightForColumn[indexForColumn]
+            
+            let offsetX = cellMargin + (columnWidth + cellMargin) * CGFloat(indexForColumn)
+            
+            if let cellHeight = delegate?.collectionView(collectionView!, layout: self, heightForCellAtIndexPath: indexPath) {
+                let frame = CGRect(x: offsetX, y: offsetY, width: columnWidth, height: cellHeight)
+                attr.frame = frame
+                layoutAttributes.append(attr)
+                heightForColumn[indexForColumn] += cellHeight + cellMargin
+            }
+        }
+    }
+    
+    override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        return layoutAttributes[indexPath.row]
+    }
+    
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        var array: [UICollectionViewLayoutAttributes] = []
+        for attr in layoutAttributes {
+            if attr.frame.intersects(rect) {
+                array.append(attr)
+            }
+        }
+        return array
+    }
+    
+    override var collectionViewContentSize: CGSize {
+        return CGSize(width: contentWidth, height: heightForColumn.max()!)
+    }
+}
